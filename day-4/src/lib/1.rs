eacd71466422ac20204df6edc621555dba69a90d
Emilio <franceschini.emilio@gmail.com>
2023-12-04 11:29:41 -0300

    create day 4

---
+++ day-4/src/lib/1.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
