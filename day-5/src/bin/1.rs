b8485dea5d9ead4cbd65d2eabbdc2318e4f9ab84
Emilio <franceschini.emilio@gmail.com>
2023-12-05 20:56:37 -0300

    day 5 part 1 finished

--- day-5/src/bin/1.rs
+++ day-5/src/bin/1.rs
@@ -3,13 +3,17 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+type Map_Seeds = (isize, isize, isize);
 fn main() {
     let mut seed_maps: Vec<HashMap<isize, isize>> = Vec::new();
+    let mut maps: Vec<Vec<Map_Seeds>> = Vec::new();
+    let mut seeds: Vec<isize> = Vec::new();
+    let mut current_map: HashMap<isize, isize> = HashMap::new();
+    let mut min_location = isize::max_value();
+    let mut reading_map = false;
+    let mut map_being_processed: Vec<Map_Seeds> = Vec::new();
     if let Ok(lines) = read_lines("./input.txt") {
         // vector of integers
-        let mut seeds: Vec<isize> = Vec::new();
-        let mut current_map: HashMap<isize, isize> = HashMap::new();
-        let mut min_location = isize::max_value();
         for (i, line) in lines.enumerate() {
             if let Ok(row) = line {
                 if i == 0 {
@@ -17,52 +21,56 @@ fn main() {
                     for seed in seeds_string.split_whitespace() {
                         seeds.push(seed.parse::<isize>().unwrap());
                     }
-                }
-                if !row.is_empty() {
-                    if row.contains("map") {
-                        if current_map.len() > 0 {
-                            seed_maps.push(current_map.clone());
-                            current_map = HashMap::new();
-                        }
-                        min_location = isize::max_value();
+                } else if row.is_empty() {
+                    if reading_map {
+                        maps.push(map_being_processed.clone());
+                        map_being_processed = Vec::new();
                     }
-                    if row.chars().collect::<Vec<char>>()[0].is_numeric() {
-                        let numbers = row.split(" ").collect::<Vec<&str>>();
-                        let destination_start = numbers[0].parse::<isize>().unwrap();
-                        let source_start = numbers[1].parse::<isize>().unwrap();
-                        let length = numbers[2].parse::<isize>().unwrap();
-                        for seed in &seeds {
-                            let mut corresponding_value_seed = seed;
-                            let mut seed_map_index = 0;
-                            while seed_maps.len() >= seed_map_index + 1
-                                && seed_maps[seed_map_index].len() > 0
-                                && seed_maps[seed_map_index].contains_key(corresponding_value_seed)
-                            {
-                                corresponding_value_seed = seed_maps[seed_map_index]
-                                    .get(corresponding_value_seed)
-                                    .unwrap();
-                                seed_map_index += 1;
-                            }
-                            // implement check *corresponding_value_seed is in range of source_start..(source_start + length -1)
-                            if (source_start..(source_start + length - 1))
-                                .contains(corresponding_value_seed)
-                            {
-                                let value_to_insert =
-                                    destination_start + (corresponding_value_seed - source_start);
-                                current_map.insert(*corresponding_value_seed, value_to_insert);
-                                if value_to_insert < min_location {
-                                    min_location = value_to_insert;
-                                }
-                            }
-                        }
-                    }
+                } else if row.contains("map") {
+                    reading_map = true;
+                } else {
+                    let numbers = row.split(" ").collect::<Vec<&str>>();
+                    let destination_start = numbers[0].parse::<isize>().unwrap();
+                    let source_start = numbers[1].parse::<isize>().unwrap();
+                    let length = numbers[2].parse::<isize>().unwrap();
+                    map_being_processed.push((destination_start, source_start, length));
                 }
             }
         }
+        maps.push(map_being_processed.clone());
 
-        seed_maps.push(current_map.clone());
-        for (key, value) in &seed_maps[1] {
-            println!("{}: {}", key, value);
+        for (index, map) in maps.iter().enumerate() {
+            min_location = isize::max_value();
+            let mut seed_map = HashMap::new();
+            for seed in &seeds {
+                let mut corresponding_value_seed = seed;
+                for i in 0..index {
+                    if seed_maps[i].contains_key(corresponding_value_seed) {
+                        corresponding_value_seed =
+                            seed_maps[i].get(corresponding_value_seed).unwrap();
+                    }
+                }
+                for numbers in map {
+                    let destination_start = numbers.0;
+                    let source_start = numbers.1;
+                    let length = numbers.2;
+                    if (source_start..(source_start + length)).contains(corresponding_value_seed) {
+                        let value_to_insert =
+                            destination_start + (corresponding_value_seed - source_start);
+                        seed_map.insert(*corresponding_value_seed, value_to_insert);
+                        if value_to_insert < min_location {
+                            min_location = value_to_insert;
+                        }
+                    }
+                }
+                if !seed_map.contains_key(corresponding_value_seed) {
+                    seed_map.insert(*corresponding_value_seed, *corresponding_value_seed);
+                    if min_location > *corresponding_value_seed {
+                        min_location = *corresponding_value_seed;
+                    }
+                }
+            }
+            seed_maps.push(seed_map.clone());
         }
         println!("{}", min_location)
     }
