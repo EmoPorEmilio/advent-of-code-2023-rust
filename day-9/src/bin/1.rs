80bd6d4fe39ca4ce0200c8c3aedc1515280f4d2c
Emilio <franceschini.emilio@gmail.com>
2023-12-11 14:16:30 -0300

    start day 9

---
+++ day-9/src/bin/1.rs
@@ -0,0 +1,22 @@
+use num::integer::lcm;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        // vector of integers
+        for (i, line) in lines.enumerate() {
+            if let Ok(row) = line {}
+        }
+    };
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
