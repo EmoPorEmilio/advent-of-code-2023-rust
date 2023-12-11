bce3025a621d200c9dc602ff0d619b0b9127bdd3
Emilio <franceschini.emilio@gmail.com>
2023-12-11 15:54:38 -0300

    part 2

---
+++ day-9/src/bin/2.rs
@@ -0,0 +1,78 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut histories: Vec<Vec<isize>> = vec![];
+    if let Ok(lines) = read_lines("./input.txt") {
+        for (i, line) in lines.enumerate() {
+            if let Ok(row) = line {
+                histories.push(
+                    row.split_whitespace()
+                        .map(|x| x.parse::<isize>().unwrap())
+                        .collect::<Vec<isize>>(),
+                );
+            }
+        }
+    };
+    let mut sum_extrapolated_values = 0;
+    for history in histories {
+        let mut differences: Vec<Vec<isize>> = vec![];
+        let mut found_zeroes = false;
+        let mut counter_steps = 0;
+        differences.push(history.clone());
+        while !found_zeroes {
+            found_zeroes = true;
+            if counter_steps == 0 {
+                let difference = history
+                    .windows(2)
+                    .map(|x| {
+                        if found_zeroes && x[1] - x[0] != 0 {
+                            found_zeroes = false;
+                        }
+                        x[1] - x[0]
+                    })
+                    .collect::<Vec<isize>>();
+                differences.push(difference);
+                counter_steps += 1;
+            } else {
+                found_zeroes = true;
+                let prev_difference = differences[differences.len() - 1].clone();
+                let difference = prev_difference
+                    .windows(2)
+                    .map(|x| {
+                        if found_zeroes && x[1] - x[0] != 0 {
+                            found_zeroes = false;
+                        }
+                        x[1] - x[0]
+                    })
+                    .collect::<Vec<isize>>();
+                differences.push(difference);
+                counter_steps += 1;
+            }
+        }
+        let mut i = differences.len() - 1;
+        let mut prev_first_digit = 0;
+        println!("Differences: {:?}", differences);
+        while i > 0 {
+            let first_digit = differences[i][0];
+            prev_first_digit = first_digit - prev_first_digit;
+            i -= 1;
+        }
+
+        let first_digit = differences[0][0];
+        prev_first_digit = first_digit - prev_first_digit;
+        println!("Prev last digit: {:?}", prev_first_digit);
+        sum_extrapolated_values += prev_first_digit;
+        println!("Sum of extrapolated values: {:?}", sum_extrapolated_values)
+    }
+    //println!("Sum of extrapolated values: {:?}", sum_extrapolated_values);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
