3022d3fd20d7817170e1bcf150cff68d76b6b3ba
Emilio <franceschini.emilio@gmail.com>
2023-12-12 15:31:53 -0300

    day 12 start

---
+++ day-12/src/bin/1.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(row) = line {
+                board.push(row.chars().collect::<Vec<char>>());
+            }
+        }
+    };
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
