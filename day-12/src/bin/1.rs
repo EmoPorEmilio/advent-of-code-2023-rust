6517bf5a30751cbfb8a2150d78cf889d968b473e
Emilio <franceschini.emilio@gmail.com>
2023-12-12 23:26:07 -0300

    missing day 12 part 1

--- day-12/src/bin/1.rs
+++ day-12/src/bin/1.rs
@@ -3,13 +3,30 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    let row_and_values: Vec<(String, Vec<usize>)> = vec![];
     if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
+        for (i, line) in lines.enumerate() {
             if let Ok(row) = line {
-                board.push(row.chars().collect::<Vec<char>>());
+                let mut new_element: (String, Vec<usize>) = (String::from(""), vec![]);
+                new_element.1 = row.split_whitespace().collect::<Vec<&str>>()[1]
+                    .split(',')
+                    .flat_map(|x| x.parse::<usize>())
+                    .collect::<Vec<usize>>();
+                new_element.0 = row.split_whitespace().collect::<Vec<&str>>()[0].to_string();
+                row_and_values.push(new_element);
+                println!("{:?}", new_element);
             }
         }
     };
+    let mut row_number = 0;
+    let mut count_valid = 0;
+    let len = row_and_values.len();
+    let current_count = 0;
+    while row_number < len {
+        let row = row_and_values[row_number];
+        for char in row.0.chars() {}
+        row_number += 1;
+    }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
